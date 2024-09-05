9e1b1a2c1000aba0561b196569fa251cc60099f2
Mohon45 <mdmohon6145@gmail.com>
2024-09-06 03:02:55 +0600

    add dummy next cripto bubbles

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import { CoingeckoCoinData } from "@/types/coingecko.type";
+import Bubbles from "./ui/Bubbles";
+export const dynamic = "force-dynamic";
+
+async function getCoins(): Promise<CoingeckoCoinData[]> {
+	// const response = await fetch(
+	// 	"https://api.coingecko.com/api/v3/" +
+	// 		"coins/markets?" +
+	// 		"vs_currency=usd" +
+	// 		"&order=market_cap_desc" +
+	// 		"&per_page=250" +
+	// 		`&page=${1}` +
+	// 		"&sparkline=true" +
+	// 		"&price_change_percentage=1h%2C24h%2C7d%2C30d%2C1y" +
+	// 		"&locale=en" +
+	// 		`&x_cg_demo_api_key=${process.env.COINGECKO_API_SECRET_KEY}`
+	// );
+	const response = await fetch(
+		"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d%2C30d%2C1y"
+		// "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc"
+	);
+
+	const data = await response.json();
+	console.log(data?.length);
+	return data;
+}
+
+export default async function Main() {
+	const coins = await getCoins();
+	return <Bubbles coins={coins} />;
+}
